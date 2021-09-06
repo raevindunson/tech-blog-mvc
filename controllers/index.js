570046e90db2f53542c83b4a9e0db078e7e49826
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-06 16:24:02 -0400

    updates to index for routes and controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,4 +2,18 @@
 const router = require("express").Router();
 
 // Controllers
-const apiRoutes = require("./api")
+const apiRoutes = require("./api");
+const dashboardRoutes = require("./dashboard-routes.js")
+const homeRoutes = require("./home-routes.js");
+
+// Routes for apis
+router.use("/api", apiRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/home", homeRoutes);
+
+// router catch error
+router.use((req, res) => {
+    res.status(404).end();
+});
+
+module.exports = router;
