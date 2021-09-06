8e8ae75a5ee9f5766cbc2ccdd80d283ebb744a38
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-05 20:07:42 -0400

    update connections and readme

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ This application uses Handlebars, MySQL2, Sequelize, express, dotenv, and bcrypt
 
 ### Assistance
 Received some help from:
+The course modules from the George Washington University Coding Bootcamp
 https://github.com/priyaravi23/mvc-tech-blog
 and https://github.com/kpehl/mvc-tech-blog
--- config/connections.js
+++ config/connections.js
@@ -1,5 +1,16 @@
-const sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-require("dotenv").config();
+require('dotenv').config();
 
-let sequelize;
+//Connect to the database
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
+
+module.exports = sequelize;
+
+// USED COURSE MODULE 14 FOR HELP
