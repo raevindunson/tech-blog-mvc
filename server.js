31ff228410958359ded4c7b8358a990058b2045d
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-05 23:47:20 -0400

    server additions

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require("express");
 const sequelize = require("path");
 const expressHandlebars = require("express-handlebars");
 const expressSession = require("express-session");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const sequelizeStore = require("connect")
