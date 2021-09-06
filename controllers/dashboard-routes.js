a0d6026a060210f8d3e516aefa4002f8899e408f
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-06 16:59:49 -0400

    updates to routes and models

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,4 +2,20 @@
 const router = require("express").Router();
 
 // Dependencies
-const sequelize = require("../config/connection")
+const sequelize = require("../config/connection");
+const {Post, User, Comment} = require("../models");
+const auth = require("../utils/auth")
+
+module.exports = router;
+
+// Routes
+router.get("/", auth, (req, res) => {
+    Post.find({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            "id",
+        ]
+    })
+})
