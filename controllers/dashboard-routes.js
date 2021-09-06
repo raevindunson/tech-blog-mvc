4405121dca5b21ed9adb6b788a1886496fd0cfad
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-06 18:52:18 -0400

    routes improvements

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -9,3 +9,46 @@ const auth = require("../utils/auth")
 module.exports = router;
 
 // Routes
+router.get("/", auth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            "id",
+            "title",
+            "content",
+            "created"
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "user_id",
+                    "post_id",
+                    "comments",
+                    "created"
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            },
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({plain: true}));
+        res.render("dashboard", {posts, loggedIn: true});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500)
+        .json(err);
+    });
+});
+
