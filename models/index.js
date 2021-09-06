165f980facf5afca9bd2c68bf5ccdb1165ab970b
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-06 17:47:38 -0400

    model index

--- models/index.js
+++ models/index.js
@@ -5,3 +5,32 @@ const Post = require("./Post");
 const User = require("./User");
 
 //Associations
+User.hasMany(Post, {
+    foreignKey: "user_id"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+    hooks: true
+});
+
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    hooks: true
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    hooks: true
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    hooks: true
+});
+
+module.exports = {User, Post, Comment};
