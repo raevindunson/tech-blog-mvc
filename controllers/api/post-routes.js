50bfbdca386c847327d51e9fad7bc95509bdd6aa
Raevin Dunson <raevin.dunson98@gmail.com>
2021-09-06 22:09:02 -0400

    api post routes updates

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -5,6 +5,7 @@ const router = require("express").Router();
 const {User, Post, Comment} = require("../../models");
 const sequelize = require("../../config/connection");
 const auth = require("../../utils/auth");
+const { where } = require("sequelize/types");
 
 // Routes
 router.get("/", (req, res) => {
@@ -42,3 +43,57 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", auth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        user_id: req.session.user_id,
+        posts: req.body.posts
+    })
+    .then((dbPostData) => {
+        res.json(dbPostData)
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500)
+        .json(err);
+    });
+});
+
+router.put("/:id", auth, (req, res) => {
+    Post.update({
+        title: req.body.title,
+        posts: req.body.posts
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404)
+            .json({MessageEvent: "There was no post found based on this id"});
+            return;
+        }})
+        .catch((err) => {
+            console.log(err);
+            res.status(500)
+            .json(err);
+        });
+});
+
+router.delete("/:id", auth, (req, res) => {
+    Post.destroy({
+        where: {id: req.params.id}
+    })
+    .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404)
+            .json({MessageEvent: "There was no post found based on this id"});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500)
+        .json(err);
+    });
+});
+
+module.exports = router;
